904380b133682887c1652d7c23018ff441eda08f
khoatm98 <khoatm98@gmail.com>
2024-10-08 22:43:53 +0800

    HW3 init

---
+++ HW3/00_TESTBED/testbench_temp.v
@@ -0,0 +1,106 @@
+`timescale 1ns/1ps
+`define CYCLE       5.0     // CLK period.
+`define HCYCLE      (`CYCLE/2)
+`define MAX_CYCLE   10000000
+`define RST_DELAY   2
+
+
+`ifdef tb1
+    `define INFILE "../00_TESTBED/PATTERN/indata1.dat"
+    `define OPFILE "../00_TESTBED/PATTERN/opmode1.dat"
+    `define GOLDEN "../00_TESTBED/PATTERN/golden1.dat"
+`elsif tb2
+    `define INFILE "../00_TESTBED/PATTERN/indata2.dat"
+    `define OPFILE "../00_TESTBED/PATTERN/opmode2.dat"
+    `define GOLDEN "../00_TESTBED/PATTERN/golden2.dat"
+`elsif tb3
+    `define INFILE "../00_TESTBED/PATTERN/indata3.dat"
+    `define OPFILE "../00_TESTBED/PATTERN/opmode3.dat"
+    `define GOLDEN "../00_TESTBED/PATTERN/golden3.dat"
+`elsif tb4
+    `define INFILE "../00_TESTBED/PATTERN/indata4.dat"
+    `define OPFILE "../00_TESTBED/PATTERN/opmode4.dat"
+    `define GOLDEN "../00_TESTBED/PATTERN/golden4.dat"
+`else
+    `define INFILE "../00_TESTBED/PATTERN/indata0.dat"
+    `define OPFILE "../00_TESTBED/PATTERN/opmode0.dat"
+    `define GOLDEN "../00_TESTBED/PATTERN/golden0.dat"
+`endif
+
+// Modify your sdf file name
+`define SDFFILE "../02_SYN/Netlist/core_syn.sdf"
+
+
+module testbed;
+
+reg         clk, rst_n;
+wire        op_valid;
+wire [ 3:0] op_mode;
+wire        op_ready;
+wire        in_valid;
+wire [ 7:0] in_data;
+wire        in_ready;
+wire        out_valid;
+wire [13:0] out_data;
+
+reg  [ 7:0] indata_mem [0:2047];
+reg  [ 3:0] opmode_mem [0:1023];
+reg  [13:0] golden_mem [0:4095];
+
+
+// ==============================================
+// TODO: Declare regs and wires you need
+// ==============================================
+
+
+// For gate-level simulation only
+`ifdef SDF
+    initial $sdf_annotate(`SDFFILE, u_core);
+    initial #1 $display("SDF File %s were used for this simulation.", `SDFFILE);
+`endif
+
+// Write out waveform file
+initial begin
+  $fsdbDumpfile("core.fsdb");
+  $fsdbDumpvars(0, "+mda");
+end
+
+
+core u_core (
+	.i_clk       (),
+	.i_rst_n     (),
+	.i_op_valid  (),
+	.i_op_mode   (),
+    .o_op_ready  (),
+	.i_in_valid  (),
+	.i_in_data   (),
+	.o_in_ready  (),
+	.o_out_valid (),
+	.o_out_data  ()
+);
+
+// Read in test pattern and golden pattern
+initial $readmemb(`INFILE, indata_mem);
+initial $readmemb(`OPFILE, opmode_mem);
+initial $readmemb(`GOLDEN, golden_mem);
+
+// Clock generation
+initial clk = 1'b0;
+always begin #(`CYCLE/2) clk = ~clk; end
+
+// Reset generation
+initial begin
+    rst_n = 1; # (               0.25 * `CYCLE);
+    rst_n = 0; # ((`RST_DELAY - 0.25) * `CYCLE);
+    rst_n = 1; # (         `MAX_CYCLE * `CYCLE);
+    $display("Error! Runtime exceeded!");
+    $finish;
+end
+
+
+// ==============================================
+// TODO: Check pattern after process finish
+// ==============================================
+
+
+endmodule
