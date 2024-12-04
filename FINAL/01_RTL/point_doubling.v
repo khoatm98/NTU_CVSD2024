5b8eaaf83e062a988f3e24b9a508f62eb1922947
LizzieEmma <Khanhlinhkstndtvtk57@gmail.com>
2024-12-04 15:26:26 +0700

    Add files via upload
    remove redundant buffer, hold input signal at each level

--- FINAL/01_RTL/point_doubling.v
+++ FINAL/01_RTL/point_doubling.v
@@ -13,15 +13,14 @@ module point_doubling #(
 	output [DATA_W-1:0]   z2
 );
 
-reg	  valid_r, i_first_lvl2_r, i_first_lvl_r, i_first_lvl_r1;
+reg	   i_first_lvl2_r, i_first_lvl_r, i_first_lvl_r1;
 reg [5:0] lvl_count;
 reg [3:0] pd_level;
-reg [255:0] A_r, B_r, C_r, D_r, E_r, F_r, J_r;
-reg [255:0] x2_r, y2_r, z2_r;
+reg [255:0] B_r, D_r, F_r, J_r;
 reg [254:0] a_addsub_r0, a_addsub_r1;
 reg [254:0] b_addsub_r0, b_addsub_r1;
 reg [255:0] a_mult_r, b_mult_r;
-reg [255:0] lvl4_res_addsub;
+//reg [255:0] lvl4_res_addsub;
 wire	  valid_pd, i_first_lvl;
 wire	  addsub_valid_0, addsub_valid_1, addsub_valid_2;
 wire [254:0] a_addsub_w0, a_addsub_w1, a_addsub_w2;
@@ -31,12 +30,9 @@ wire [255:0] res_addsub_w0, res_addsub_w1, res_addsub_w2, res_mult_w;
 wire z1 = 1'b1;
 
 always @(posedge i_clk) begin
-	if (i_rst) begin
-		A_r 		<= 0;
+	if (i_rst) begin	
 		B_r 		<= 0;
-		C_r 		<= 0;
 		D_r 		<= 0;
-		E_r 		<= 0;
 		F_r 		<= 0;
 		J_r 		<= 0;
 		a_addsub_r0 	<= 0;
@@ -49,67 +45,85 @@ always @(posedge i_clk) begin
 	end else begin
 		case (pd_level) 
 			0: begin				//level 1
-				a_addsub_r0 <= x1;		//output: B
-				b_addsub_r0 <= y1;		//output: B
-				a_mult_r    <= y1;		//output: D
-		 		b_mult_r    <= y1;		//output: D
+				if (i_first_lvl_r) begin 	//i_first_lvl_r 1 cycle earlier than i_first_lvl
+					a_addsub_r0 <= x1;		//output: B
+					b_addsub_r0 <= y1;		//output: B
+					a_mult_r    <= y1;		//output: D
+			 		b_mult_r    <= y1;		//output: D
+				end
 			end	
 			1: begin
-                                a_mult_r    <= x1; 		//output: C
-                                b_mult_r    <= x1; 		//output: C
-				B_r	    <= res_addsub_w0;	//temporary save B
+				if (i_first_lvl_r) begin
+	                                a_mult_r    <= x1; 		//output: C
+	                                b_mult_r    <= x1; 		//output: C
+					B_r	    <= res_addsub_w0;	//temporary save B
+				end
 				if (i_first_lvl) begin
 					D_r	    <= res_mult_w;	//temporary save D
 				end
                         end 
 			2: begin
-                                a_addsub_r0 <= res_mult_w;	//input: C 
-                                b_addsub_r0 <= D_r; 		//input: D
-				a_addsub_r1 <= res_mult_w;	//input: C, output: F
-				b_addsub_r1 <= D_r;		//input: D, output: F
-                                a_mult_r    <= B_r;	 	//input: B 
-                                b_mult_r    <= B_r;	 	//input: B
+				if (i_first_lvl_r) begin
+	                                a_addsub_r0 <= res_mult_w;	//input: C 
+	                                b_addsub_r0 <= D_r; 		//input: D
+					a_addsub_r1 <= res_mult_w;	//input: C, output: F
+					b_addsub_r1 <= D_r;		//input: D, output: F
+	                                a_mult_r    <= B_r;	 	//input: B 
+	                                b_mult_r    <= B_r;	 	//input: B
+				end
 				i_first_lvl2_r <= addsub_valid_1;
                         end 
                         3: begin
-                                a_addsub_r0 <= res_mult_w;
-                                b_addsub_r0 <= res_addsub_w2;	//input: E
-                                a_mult_r    <= z1;		//input: Z
-                                b_mult_r    <= z1;		//input: Z
+				if (i_first_lvl_r) begin
+	                                a_addsub_r0 <= res_mult_w;
+	                                b_addsub_r0 <= res_addsub_w2;	//input: E
+	                                a_mult_r    <= z1;		//input: Z
+	                                b_mult_r    <= z1;		//input: Z
+				end
 				if (i_first_lvl) begin
 					F_r	    <= res_addsub_w1;	//temporary save F
 				end	
                         end 
 			4: begin
-                                a_addsub_r0 <= res_mult_w;	//output: A 
-                                b_addsub_r0 <= res_mult_w; 	//output: A
+				if (i_first_lvl_r) begin
+	                                a_addsub_r0 <= res_mult_w;	//output: A 
+	                                b_addsub_r0 <= res_mult_w; 	//output: A
+				end
 				if (i_first_lvl) begin
-					lvl4_res_addsub <= res_addsub_w0; //temporary save add in level 4
+					B_r <= res_addsub_w0; //temporary save add at level 4 in B
                         	end
 			end 
                         5: begin
-                                a_addsub_r1 <= F_r;		//input: F, output: J
-                                b_addsub_r1 <= res_addsub_w0;
-                                a_mult_r    <= res_addsub_w2;	//input: E;
-                                b_mult_r    <= F_r;		
+				if (i_first_lvl_r) begin
+	                                a_addsub_r1 <= F_r;		//input: F, output: J
+	                                b_addsub_r1 <= res_addsub_w0;
+	                                a_mult_r    <= res_addsub_w2;	//input: E;
+	                                b_mult_r    <= F_r;	
+				end	
                         end 
                         6: begin
-                                a_mult_r    <= lvl4_res_addsub;
-                                b_mult_r    <= res_addsub_w1;
-				if (i_first_lvl) begin
-					y2_r	    <= res_mult_w;
+				if (i_first_lvl_r) begin
+	                                a_mult_r    <= B_r;
+	                                b_mult_r    <= res_addsub_w1;
 				end
-				J_r	    <= res_addsub_w1;
+				if (i_first_lvl) begin
+					D_r	    <= res_mult_w;
+					J_r         <= res_addsub_w1;
+				end
                         end
                         7: begin
-                                a_mult_r    <= J_r;
-                                b_mult_r    <= F_r;
+			 	if (i_first_lvl_r) begin	
+	                                a_mult_r    <= J_r;
+	                                b_mult_r    <= F_r;
+				end
 				if (i_first_lvl) begin
-					x2_r	    <= res_mult_w;
+					B_r	    <= res_mult_w;
 				end
 			end
 			8: begin
-				z2_r	    <= res_mult_w;
+				if (i_first_lvl) begin
+					J_r	    <= res_mult_w;
+				end
                         end
 		endcase
 	end	
@@ -152,9 +166,9 @@ assign b_addsub_w1 = b_addsub_r1;
 assign b_addsub_w2 = (pd_level == 2) ? res_addsub_w0 : 0;
 assign a_mult_w	   = a_mult_r;
 assign b_mult_w	   = b_mult_r;
-assign x2	   = x2_r;
-assign y2	   = y2_r;
-assign z2	   = z2_r;
+assign x2	   = B_r;
+assign y2	   = D_r;
+assign z2	   = J_r;
 assign o_valid	   = (pd_level == 8) ? i_first_lvl : 0;
 
 modular_add_sub modular_add_sub_pd_inst0 (
